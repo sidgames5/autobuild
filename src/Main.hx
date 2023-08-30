a555a8b469f1e51453f007aa58d1ff3f049bf3a5
Sid <saytleader@outlook.com>
2023-08-30 07:21:31 -0400

    Update the script

--- src/Main.hx
+++ src/Main.hx
@@ -1,5 +1,17 @@
+import hscript.Interp;
+import sys.io.File;
+import hscript.Parser;
+
+using StringTools;
+
 class Main {
 	static function main() {
-		trace("Hello, world!");
+		var script = File.getContent("examples/Autobuild");
+
+		for (line in script.split("\n")) {
+			if (line.startsWith("#")) {} else {
+				Sys.command(line);
+			}
+		}
 	}
 }
